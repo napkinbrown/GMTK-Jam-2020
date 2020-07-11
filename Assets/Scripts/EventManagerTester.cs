a8fe211084e962728f47799ce2f9d52925fa7b17
Dominic Brown <domingomonk@gmail.com>
2020-07-10 21:40:00 -0400

    Added EventManager and test scene

---
+++ Assets/Scripts/EventManagerTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManagerTester : MonoBehaviour
+{
+    public readonly string TEST_EVENT = "Test Event";
+
+    void OnEnable() {
+        EventManager.StartListening(TEST_EVENT, TestEventAction);
+    }
+
+    void OnDisable() {
+        EventManager.StopListening(TEST_EVENT, TestEventAction);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space")) {
+            Debug.Log("Event fired!");
+            EventManager.TriggerEvent(TEST_EVENT);
+        }
+    }
+
+    void TestEventAction() {
+        Debug.Log("Event heard!");
+    }
+}
