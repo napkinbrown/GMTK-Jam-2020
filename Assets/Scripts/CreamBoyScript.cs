46dc1339435b553cebd75721e202ebde92b66c3a
Matt Brown <matt.brown2@gmail.com>
2020-07-11 00:39:13 -0500

    Initial try at shooting sprinkles

---
+++ Assets/Scripts/CreamBoyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreamBoyScript : MonoBehaviour
+{
+    public float rateOfFire;
+    public int shotSpeed = 50;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void Fire()
+     {
+         // Need to rate limit
+         // Have to create a bullet
+         // Have to make sure that bullet has physics
+
+        GameObject go = Instantiate(projectile, gameObject.transform);
+        SprinkleScript bullet = go.GetComponent<SprinkleScript>();
+        bullet.targetVector = new Vector3(1,1,0);
+        bullet.speed = shotSpeed;
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Change to GetButton when we start rate limiting
+        if (Input.GetButtonDown("Fire1")){
+             Fire();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SprinkleScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprinkleScript : MonoBehaviour
+{
+    public int speed = 50;          // The speed our bullet travels
+    public Vector3 targetVector;    // the direction it travels
+    public float lifetime = 10f;     // how long it lives before destroying itself
+    public float damage = 10;       // how much damage this projectile causes
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite sprinkle1;
+    public Sprite sprinkle2;
+    public Sprite sprinkle3;
+    public Sprite sprinkle4;
+    private bool hasAColor = false;
+ 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PickStartColor();
+ 
+        
+         // find our RigidBody
+        Rigidbody2D rb = gameObject.GetComponentInChildren<Rigidbody2D>();
+        // add force 
+        rb.AddForce(targetVector.normalized * speed);
+    }
+
+    void PickStartColor()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        switch (Random.Range(1, 5))
+        {
+            case 1:
+                spriteRenderer.sprite = Instantiate(sprinkle2);
+                break;
+            case 2:
+                spriteRenderer.sprite = Instantiate(sprinkle3);
+                break;
+            case 3:
+                spriteRenderer.sprite = Instantiate(sprinkle4);
+                break;
+            case 4:
+                //Default is already set in the prefab
+                Debug.Log("we shot the shot");
+                spriteRenderer.sprite = Instantiate(sprinkle1);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(!hasAColor){
+        //     PickStartColor();
+        //     hasAColor = true;
+        // }
+        // decrease our life timer
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0f) {
+            // we have ran out of life
+            Destroy(gameObject);    // kill me
+        }
+    }
+}
+
+
