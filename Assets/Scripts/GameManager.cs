f7a6d95909ce485c8b9057dfdca1ad1921f4bd0f
Matt Brown <matt.brown2@gmail.com>
2020-07-12 11:22:26 -0500

    Disply death message then restart scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public YouDied deathMessage;
+     void OnEnable() {
+        EventManager.StartListening(EventNames.PLAYER_DIED, HeDead);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnDisable() {
+        EventManager.StopListening(EventNames.PLAYER_DIED, HeDead);
+    }
+    
+    void HeDead() {
+        deathMessage.GetComponent<Text>().enabled = true;
+        Invoke("RestartGame", 5f);
+    }
+
+    void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
