9ccbc3de68c84d303786fb5dc77cee90495066bc
unknown <teague.dnq@gmail.com>
2020-07-12 09:03:47 -0400

    adding spawn controller

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject pizzaEnemy;
+    public GameObject broccoliEnemy;
+    public GameObject spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
